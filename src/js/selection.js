fafd28b69aa396389d90400981c5a415f0bfc98b
nicolas piquet <nicolas.piquet@epfedu.fr>
2024-03-07 12:52:51 +0100

    mise en place portail

--- src/js/selection.js
+++ src/js/selection.js
@@ -27,34 +27,38 @@ export default class selection extends Phaser.Scene {
   preload() {
     // tous les assets du jeu sont placés dans le sous-répertoire src/assets/
     this.load.image("img_paysage", "src/assets/assets_bienvenue/image_ecran_bienvenue.png");
-    
+
     this.load.image("img_plateforme", "src/assets/assets_bienvenue/platform.png");
     this.load.spritesheet("img_perso", "src/assets/dude.png", {
       frameWidth: 48,
       frameHeight: 48
     });
-    this.load.image("img_porte1", "src/assets/assets_bienvenue/door1.png");
-    this.load.image("img_porte2", "src/assets/assets_bienvenue/door2.png");
-    this.load.image("img_porte3", "src/assets/assets_bienvenue/door3.png");
-    this.load.audio('musique_background', 'src/assets/musiques/zoo_kaaris.mp3');   
+    this.load.spritesheet('img_portail', 'src/assets/assets_bienvenue/spritesheet_portail.png', {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    //this.load.image("img_porte1", "src/assets/assets_bienvenue/door1.png");
+    //this.load.image("img_porte2", "src/assets/assets_bienvenue/door2.png");
+    //this.load.image("img_porte3", "src/assets/assets_bienvenue/door3.png");
+    this.load.audio('musique_background', 'src/assets/musiques/zoo_kaaris.mp3');
   }
 
-  
-  create() {
-      fct.doNothing();
-      fct.doAlsoNothing();
-      var musique_de_fond;
-      musique_de_fond = this.sound.add('musique_background');
-      musique_de_fond.volume = 0.2;
-      // Définir l'option loop sur true
-      var config = {
-        loop: true,
-        volume: 0.2
-      };
 
-      // Jouer la musique avec la configuration spécifiée
-      musique_de_fond.play(config); 
-      this.musique_de_fond = musique_de_fond;
+  create() {
+    fct.doNothing();
+    fct.doAlsoNothing();
+    var musique_de_fond;
+    musique_de_fond = this.sound.add('musique_background');
+    musique_de_fond.volume = 0.2;
+    // Définir l'option loop sur true
+    var config = {
+      loop: true,
+      volume: 0.2
+    };
+
+    // Jouer la musique avec la configuration spécifiée
+    musique_de_fond.play(config);
+    this.musique_de_fond = musique_de_fond;
     /*************************************
      *  CREATION DU MONDE + PLATEFORMES  *
      *************************************/
@@ -84,17 +88,15 @@ export default class selection extends Phaser.Scene {
     /****************************
      *  Ajout des portes   *
      ****************************/
-    this.porte1 = this.physics.add.staticSprite(600, 414, "img_porte1");
-    //this.porte2 = this.physics.add.staticSprite(50, 264, "img_porte2");
-    //this.porte3 = this.physics.add.staticSprite(775, 234, "img_porte3");
+    this.porte1 = this.physics.add.staticSprite(600, 414, "img_portail");
 
 
     player = this.physics.add.sprite(100, 450, "img_perso");
-    player.direction = 'right';  
+    player.direction = 'right';
     player.setBounce(0.2); // on donne un petit coefficient de rebond
     player.setCollideWorldBounds(true); // le player se cognera contre les bords du monde
-    
-    player.setSize(20,48);
+
+    player.setSize(20, 48);
     /***************************
      *  CREATION DES ANIMATIONS *
      ****************************/
@@ -142,111 +144,122 @@ export default class selection extends Phaser.Scene {
       50,
       "Bienvenue dans l'épopée de Dorelys !",
       {
-          font: "bold 36px Arial",
-          fill: "#ffffff",
-          stroke: "#ffA500",
-          strokeThickness: 6
+        font: "bold 36px Arial",
+        fill: "#ffffff",
+        stroke: "#ffA500",
+        strokeThickness: 6
       }
-  );
-  bienvenueTexte.setOrigin(0.5, 0);
+    );
+    bienvenueTexte.setOrigin(0.5, 0);
 
-  // Ajout du bouton "Règles du jeu"
-var reglesBouton = this.add.text(
-  this.cameras.main.width / 2,
-  100,
-  "Règles du jeu",
-  {
-      font: "bold 24px Arial",
-      fill: "#ffffff",
-      stroke: "#ffA500",
-      strokeThickness: 4
-  }
-);
-reglesBouton.setOrigin(0.5, -0.8);
-reglesBouton.setInteractive();
+    // Ajout du bouton "Règles du jeu"
+    var reglesBouton = this.add.text(
+      this.cameras.main.width / 2,
+      100,
+      "Règles du jeu",
+      {
+        font: "bold 24px Arial",
+        fill: "#ffffff",
+        stroke: "#ffA500",
+        strokeThickness: 4
+      }
+    );
+    reglesBouton.setOrigin(0.5, -0.8);
+    reglesBouton.setInteractive();
 
-// Ajouter un événement de survol au bouton "Règles du jeu"
-reglesBouton.on('pointerover', function () {
-  reglesBouton.setScale(1.2);  // Grossir le texte lors du survol
-});
+    // Ajouter un événement de survol au bouton "Règles du jeu"
+    reglesBouton.on('pointerover', function () {
+      reglesBouton.setScale(1.2);  // Grossir le texte lors du survol
+    });
 
-// Ajouter un événement de survol au bouton "Règles du jeu"
-reglesBouton.on('pointerout', function () {
-  reglesBouton.setScale(1);  // Rétablir la taille normale lorsque la souris quitte
-});
+    // Ajouter un événement de survol au bouton "Règles du jeu"
+    reglesBouton.on('pointerout', function () {
+      reglesBouton.setScale(1);  // Rétablir la taille normale lorsque la souris quitte
+    });
 
-// Ajouter un événement de clic au bouton
-reglesBouton.on('pointerdown', function () {
-  this.scene.launch('reglesScene'); // 'reglesScene' est le nom de la nouvelle scène
-}, this);
+    // Ajouter un événement de clic au bouton
+    reglesBouton.on('pointerdown', function () {
+      this.scene.launch('reglesScene'); // 'reglesScene' est le nom de la nouvelle scène
+    }, this);
 
-  // Créez une nouvelle scène pour les règles du jeu
-var reglesScene = new Phaser.Scene('reglesScene');
+    // Créez une nouvelle scène pour les règles du jeu
+    var reglesScene = new Phaser.Scene('reglesScene');
 
-reglesScene.create = function () {
+    // creation de l'animation pour faire tourner le portail
+    this.anims.create({
+      key: "anim_portail",
+      frames: this.anims.generateFrameNumbers("img_portail", {
+        start: 0,
+        end: 7
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
 
-    // Fond bleu foncé
-    var fond = this.add.rectangle(
+    reglesScene.create = function () {
+
+      // Fond bleu foncé
+      var fond = this.add.rectangle(
         this.cameras.main.width / 2,
         this.cameras.main.height / 2,
         this.cameras.main.width,
         this.cameras.main.height,
         0x000033
-    );
-    fond.setOrigin(0.5);
+      );
+      fond.setOrigin(0.5);
 
-    // Texte des règles
-    var reglesTexte = this.add.text(
+      // Texte des règles
+      var reglesTexte = this.add.text(
         this.cameras.main.width / 2,
         this.cameras.main.height / 2 - 50,
-        "Règles du jeu\n\nVous êtes Dorelys, une petite fille née hier.\nVous devez récupérer la grosse étoile en peluche !\nIl vous faut donc traverser les trois niveaux pour\nespérer les revoir. Attention, des monstres sont là\npour vous bouffer donc équipez-vous d'une\narme dès que vous le pouvez !",
+        "Règles du jeu\n\nVous êtes Dorelys, une petite fille née hier.\nVous devez récupérer la grosse étoile en peluche !\nIl vous faut donc traverser les trois niveaux pour\nla trouver. Attention, des monstres sont là\npour vous bouffer donc équipez-vous d'une\narme dès que vous le pouvez !\nC'est parti, rendez-vous au portail et appuyez sur espace !",
         {
-            font: "bold 24px Arial",
-            fill: "#ffffff",
-            stroke: "null",
-            align: 'center'
+          font: "bold 24px Arial",
+          fill: "#ffffff",
+          stroke: "null",
+          align: 'center'
         }
-    );
-    reglesTexte.setOrigin(0.5);
+      );
+      reglesTexte.setOrigin(0.5);
 
 
-// Ajouter un bouton "Fermer" pour revenir à la scène principale
-var fermerBouton = this.add.text(
-  this.cameras.main.width / 2,
-  this.cameras.main.height - 50,
-  "Fermer",
-  {
-      font: "bold 20px Arial",
-      fill: "#ffffff",
-      stroke: "#ffA500",
-      strokeThickness: 3
-  }
-);
-fermerBouton.setOrigin(0.5);
-fermerBouton.setInteractive();
+      // Ajouter un bouton "Fermer" pour revenir à la scène principale
+      var fermerBouton = this.add.text(
+        this.cameras.main.width / 2,
+        this.cameras.main.height - 50,
+        "Fermer",
+        {
+          font: "bold 20px Arial",
+          fill: "#ffffff",
+          stroke: "#ffA500",
+          strokeThickness: 3
+        }
+      );
+      fermerBouton.setOrigin(0.5);
+      fermerBouton.setInteractive();
 
 
-    // Ajouter un événement de survol au bouton "Fermer"
-fermerBouton.on('pointerover', function () {
-  fermerBouton.setScale(1.2);  // Grossir le texte lors du survol
-});
+      // Ajouter un événement de survol au bouton "Fermer"
+      fermerBouton.on('pointerover', function () {
+        fermerBouton.setScale(1.2);  // Grossir le texte lors du survol
+      });
 
-// Ajouter un événement de survol au bouton "Fermer"
-fermerBouton.on('pointerout', function () {
-  fermerBouton.setScale(1);  // Rétablir la taille normale lorsque la souris quitte
-});
+      // Ajouter un événement de survol au bouton "Fermer"
+      fermerBouton.on('pointerout', function () {
+        fermerBouton.setScale(1);  // Rétablir la taille normale lorsque la souris quitte
+      });
 
-// Ajouter un événement de clic au bouton "Fermer"
-fermerBouton.on('pointerdown', function () {
-  this.scene.stop('reglesScene');  // Ferme la scène des règles
-}, this);  
-}
+      // Ajouter un événement de clic au bouton "Fermer"
+      fermerBouton.on('pointerdown', function () {
+        this.scene.stop('reglesScene');  // Ferme la scène des règles
+      }, this);
+    }
 
-// Ajoutez la nouvelle scène au gestionnaire de scènes de Phaser
-this.scene.add('reglesScene', reglesScene);
+    // Ajoutez la nouvelle scène au gestionnaire de scènes de Phaser
+    this.scene.add('reglesScene', reglesScene);
 
-  
-};
+
+  };
 
 
 
@@ -257,7 +270,7 @@ this.scene.add('reglesScene', reglesScene);
 
 
   update() {
-    
+
     if (clavier.left.isDown) {
       player.setVelocityX(-160);
       player.anims.play("anim_tourne_gauche", true);
@@ -275,16 +288,6 @@ this.scene.add('reglesScene', reglesScene);
       if (this.physics.overlap(player, this.porte1))
         //this.musique_de_fond.stop();
         this.scene.switch("niveau1");
-      /**if (this.physics.overlap(player, this.porte2))
-        //this.musique_de_fond.stop(); 
-        this.scene.switch("niveau2");
-      if (this.physics.overlap(player, this.porte3))
-        //this.musique_de_fond.stop();
-        this.scene.switch("niveau3");*/
     }
   }
 }
-
-/***********************************************************************/
-/** CONFIGURATION GLOBALE DU JEU ET LANCEMENT 
-/***********************************************************************/
